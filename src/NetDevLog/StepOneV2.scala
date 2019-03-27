5485e8ab4d6dcd84a62e495ff64c899f3325d77f
heyuantao <he_yuan_tao@163.com>
2019-03-27 22:14:28 +0800

    spark context

--- src/NetDevLog/StepOneV2.scala
+++ src/NetDevLog/StepOneV2.scala
@@ -7,9 +7,9 @@ import org.apache.log4j.{Level,Logger}
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.types.{StructType,StructField,StringType}
 
-case class Record(date:String,action:String,source_ip:String,source_port:String,dest_ip:String,dest_port:String)
+case class OneItem(date:String,action:String,source_ip:String,source_port:String,dest_ip:String,dest_port:String)
 
-object StepOne {
+object StepOneV2 {
   def main(args: Array[String]) {
     Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
     Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.OFF)
@@ -22,17 +22,15 @@ object StepOne {
     val conf = new SparkConf().setAppName("StepOne").setMaster(args(0))
     //val sparkContext = new SparkContext(conf)
     val sparkSession = SparkSession.builder().config(conf).getOrCreate()
-    //import sparkSession.implicits._
+    import sparkSession.implicits._
     val sparkContext = sparkSession.sparkContext
 
-    val schemaString = "data action source_ip source_port dest_ip dest_port"
-    val schema = StructType(schemaString.split(" ").map(fieldName => StructField(fieldName, StringType)))
     val textFile = sparkContext.textFile(inputFile)
 
-    val rowRDD = textFile.map(_.split(" ")).map(p=>Row(p(0),p(1),p(2),p(3),p(4),p(5)))
-    val logDataFrame = sparkSession.createDataFrame(rowRDD, schema)
+    val logDF = textFile.map(_.split(" ")).map(p=>OneItem(p(0),p(1),p(2),p(3),p(4),p(5))).toDF()
+    //val logDataFrame = sparkSession.createDataFrame(rowRDD, schema)
 
-    logDataFrame.createOrReplaceTempView("log")
+    logDF.createOrReplaceTempView("log")
 
     val display_rdd = sparkSession.sql("select source_ip,dest_ip from log where  dest_port=22").rdd
     display_rdd.take(10).foreach(println)
