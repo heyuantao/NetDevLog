7e2a33bf26667f8eef802c40a56df71b3c89c982
heyuantao <he_yuan_tao@163.com>
2019-03-27 22:31:57 +0800

    spark context

--- src/NetDevLog/StepOne.scala
+++ src/NetDevLog/StepOne.scala
@@ -1,7 +1,4 @@
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.SQLContext
 import org.apache.spark.SparkConf
 import org.apache.log4j.{Level,Logger}
 import org.apache.spark.sql.Row
--- src/NetDevLog/StepOneV2.scala
+++ src/NetDevLog/StepOneV2.scala
@@ -1,11 +1,6 @@
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.SQLContext
 import org.apache.spark.SparkConf
 import org.apache.log4j.{Level,Logger}
-import org.apache.spark.sql.Row
-import org.apache.spark.sql.types.{StructType,StructField,StringType}
 
 case class OneItem(date:String,action:String,source_ip:String,source_port:String,dest_ip:String,dest_port:String)
 
@@ -20,19 +15,21 @@ object StepOneV2 {
     }
     val inputFile =  args(1)
     val conf = new SparkConf().setAppName("StepOne").setMaster(args(0))
-    //val sparkContext = new SparkContext(conf)
     val sparkSession = SparkSession.builder().config(conf).getOrCreate()
-    import sparkSession.implicits._
     val sparkContext = sparkSession.sparkContext
+
+    import sparkSession.implicits._
 
     val textFile = sparkContext.textFile(inputFile)
 
     val logDF = textFile.map(_.split(" ")).map(p=>OneItem(p(0),p(1),p(2),p(3),p(4),p(5))).toDF()
-    //val logDataFrame = sparkSession.createDataFrame(rowRDD, schema)
 
     logDF.createOrReplaceTempView("log")
 
-    val display_rdd = sparkSession.sql("select source_ip,dest_ip from log where  dest_port=22").rdd
-    display_rdd.take(10).foreach(println)
+    val display_rdd1 = sparkSession.sql("select source_ip,dest_ip from log where  dest_port=22").rdd
+    display_rdd1.take(30).foreach(println)
+    println("################################")
+    val display_rdd2 = sparkSession.sql("select source_ip,dest_ip from log where  dest_port=80").rdd
+    display_rdd2.take(30).foreach(println)
   }
 }
