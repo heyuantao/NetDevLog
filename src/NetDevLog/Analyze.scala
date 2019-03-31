02336d3ed4eba468943c5c2cb4edc380a231c7f9
heyuantao <he_yuan_tao@163.com>
2019-03-31 13:24:19 +0800

    save result to db success

--- src/NetDevLog/Analyze.scala
+++ src/NetDevLog/Analyze.scala
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.functions._
@@ -26,13 +28,22 @@ object Analyze {
 
   def topAccess(sc:SparkSession,df:DataFrame): Unit ={
     df.createOrReplaceTempView("access")
-    val query_cmd="""
-      select source_port,count(source_port) from access where dest_ip='151.101.228.167' group by source_port
-        order by source_port
-      """
-    //val query_cmd = "select * from access where dest_ip='151.101.228.167' "
+    //val query_cmd="""
+    //  select source_port,count(source_port) from access where dest_ip='151.101.228.167' group by source_port
+    //    order by source_port
+    //  """
+    val query_cmd = "select * from access where dest_ip='151.101.228.167' "
     val result = sc.sql(query_cmd)
     result.show(false)
+
+    var connectionProperties = new Properties();
+    connectionProperties.put("driver", "com.mysql.jdbc.Driver");
+    connectionProperties.put("url", "jdbc:mysql://localhost:3306/spark");
+    connectionProperties.put("user", "root");
+    //connectionProperties.put("password", "password");
+    result.write.mode("append")
+      .jdbc("jdbc:mysql://localhost:3306/spark","spark.result",connectionProperties)
+    //停止SparkContext
   }
 
 }
